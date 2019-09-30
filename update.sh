346d798751210ce9de9392d86a75b812c1310a66
Jeffrey Macko <github.jm@macko.fr>
2019-09-30 11:49:49 +0200

    update

---
+++ update.sh
@@ -0,0 +1,22 @@
+# Set version
+declare SWAGGEN_VERSION="$(cat VERSION)"
+
+# Build
+docker build                                   \
+--build-arg SWAGGEN_VERSION=${SWAGGEN_VERSION} \
+--force-rm                                     \
+--pull                                         \
+--tag mackoj/swaggen-docker:${SWAGGEN_VERSION}      \
+.
+
+# Test
+docker run --rm mackoj/swaggen-docker:${SWAGGEN_VERSION} swaggen --version
+
+# Push
+docker login
+docker tag mackoj/swaggen-docker:${SWAGGEN_VERSION} mackoj/swaggen-docker:latest
+docker push mackoj/swaggen-docker:${SWAGGEN_VERSION}
+docker push mackoj/swaggen-docker:latest
+
+# Tag docker repo
+git tag "v${SWAGGEN_VERSION}"
