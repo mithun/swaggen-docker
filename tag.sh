b34e925f9f0fb0f52cfbe3ffb480972b5167ac6a
Jeffrey MACKO <github.jm@macko.fr>
2019-10-20 23:36:43 +0200

    tag work as expected

--- tag.sh
+++ tag.sh
@@ -1,1 +1,10 @@
+# Set version
+declare SWAGGEN_VERSION="4.2.0"
+declare MAINTAINER="original maintainer - Mithun Ayachit <m0t0rbr3th@gmail.com>"
+declare SWAGGEN_REPO="https://github.com/yonaskolb/SwagGen"
+declare DOCKER_USER="hawkci"
+declare DOCKER_PROJECT="swaggen"
+# declare SWIFT_VERSION="swift:5.1.1"
+
+
 git tag "${SWAGGEN_VERSION}"
