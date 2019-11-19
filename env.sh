3726c735325b20b376b719e473220b87beaa35fd
Jeffrey Macko <github.jm@macko.fr>
2019-11-19 16:47:08 +0100

    More variables

--- env.sh
+++ env.sh
@@ -5,4 +5,4 @@ declare SWAGGEN_REPO="https://github.com/yonaskolb/SwagGen"
 declare DOCKER_USER="hawkci"
 declare DOCKER_PROJECT="swaggen"
 declare DOCKER_VERSION="4.3.0-slim"
-
+declare DOCKER_VERSION_ALIAS="latest"
