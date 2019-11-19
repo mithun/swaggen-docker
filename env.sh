f3f3d5b1ecfb76cc3159ef1acf895c0dca918cdd
Jeffrey Macko <github.jm@macko.fr>
2019-11-19 16:27:37 +0100

    Update to 4.3.0

--- env.sh
+++ env.sh
@@ -4,5 +4,5 @@ declare MAINTAINER="Jeffrey MACKO(@jeffreymacko) - Original maintainer: Mithun A
 declare SWAGGEN_REPO="https://github.com/yonaskolb/SwagGen"
 declare DOCKER_USER="hawkci"
 declare DOCKER_PROJECT="swaggen"
-declare DOCKER_VERSION="4.2.0-slim"
+declare DOCKER_VERSION="4.3.0-slim"
 
