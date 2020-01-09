f3907f0c6b651ccde2585c8eefb4d06b6637c8b2
Jeffrey Macko <github.jm@macko.fr>
2020-01-09 13:58:35 +0100

    NFC only build when new version of Swift or Swaggen arise

--- build.sh
+++ build.sh
@@ -1,12 +1,26 @@
 . ./env.sh
 
-# Build
-docker build                               						\
---build-arg DOCKER_VERSION="${DOCKER_VERSION}"					\
---build-arg SWAGGEN_VERSION="${SWAGGEN_VERSION}"				\
---build-arg SWAGGEN_REPO="${SWAGGEN_REPO}"						\
---build-arg MAINTAINER="${MAINTAINER}"							\
---force-rm														\
---pull															\
---tag "${DOCKER_USER}/${DOCKER_PROJECT}:${DOCKER_VERSION}"		\
-.
+./getLastSwiftTag.swift "apple/swift" "-RELEASE" NEW_SWIFT_VERSION
+
+LAST_SWIFT_VERSION="$(cat NEW_SWIFT_VERSION)"
+OLD_LAST_SWAGGEN_VERSION="$(cat LAST_SWAGGEN_VERSION)"
+OLD_LAST_SWIFT_VERSION="$(cat LAST_SWIFT_VERSION)"
+
+if [[ "${LAST_SWAGGEN_VERSION}" != LAST_SWAGGEN_VERSION ]] && [[ "${LAST_SWIFT_VERSION}" != OLD_LAST_SWIFT_VERSION ]]; then
+    # Build
+    docker build                                                    \
+    --build-arg DOCKER_VERSION="${DOCKER_VERSION}"                  \
+    --build-arg SWAGGEN_VERSION="${SWAGGEN_VERSION}"                \
+    --build-arg SWAGGEN_REPO="${SWAGGEN_REPO}"                      \
+    --build-arg MAINTAINER="${MAINTAINER}"                          \
+    --force-rm                                                      \
+    --pull                                                          \
+    --tag "${DOCKER_USER}/${DOCKER_PROJECT}:${DOCKER_VERSION}"      \
+    .
+
+    echo "${LAST_SWAGGEN_VERSION}" > LAST_SWAGGEN_VERSION
+    echo "${LAST_SWIFT_VERSION}" > LAST_SWIFT_VERSION
+else
+  echo "No need to update"
+fi
+
--- env.sh
+++ env.sh
@@ -1,11 +1,10 @@
 # Set version
 declare REPO="yonaskolb/SwagGen"
-LASTVERSION="$(curl --silent "https://api.github.com/repos/${REPO}/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
+declare LAST_SWAGGEN_VERSION="$(curl --silent "https://api.github.com/repos/${REPO}/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
 declare SWAGGEN_VERSION="$(cat VERSION)"
 declare MAINTAINER="Jeffrey MACKO(@jeffreymacko) - Original maintainer: Mithun Ayachit <m0t0rbr3th@gmail.com>"
 declare SWAGGEN_REPO="https://github.com/${REPO}"
 declare DOCKER_USER="hawkci"
 declare DOCKER_PROJECT="swaggen"
-declare DOCKER_VERSION="${LASTVERSION}-slim"
+declare DOCKER_VERSION="${LAST_SWAGGEN_VERSION}-slim"
 declare DOCKER_VERSION_ALIAS="latest"
-
