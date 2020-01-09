4979305a63ee652db8b28c3f016e8ec3f99abe36
Jeffrey Macko <github.jm@macko.fr>
2020-01-09 12:25:14 +0100

    Get automatically last version

--- env.sh
+++ env.sh
@@ -1,8 +1,11 @@
 # Set version
+declare REPO="yonaskolb/SwagGen"
+LASTVERSION="$(curl --silent "https://api.github.com/repos/${REPO}/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
 declare SWAGGEN_VERSION="$(cat VERSION)"
 declare MAINTAINER="Jeffrey MACKO(@jeffreymacko) - Original maintainer: Mithun Ayachit <m0t0rbr3th@gmail.com>"
-declare SWAGGEN_REPO="https://github.com/yonaskolb/SwagGen"
+declare SWAGGEN_REPO="https://github.com/${REPO}"
 declare DOCKER_USER="hawkci"
 declare DOCKER_PROJECT="swaggen"
-declare DOCKER_VERSION="4.3.0-slim"
+declare DOCKER_VERSION="${LASTVERSION}-slim"
 declare DOCKER_VERSION_ALIAS="latest"
+
